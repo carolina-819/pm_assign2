6ace7d1ba4d6503185ee7ffdc756f3f48bab6726
Carolina <up201705692@fe.up.pt>
2021-12-05 14:36:09 +0000

    nao funciona

--- include/object_3d_estimation.h
+++ include/object_3d_estimation.h
@@ -1,17 +1,19 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <sensor_msgs/Image.h>
- #include <pcl_ros/point_cloud.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl_ros/transforms.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/features2d.hpp>
-#include "pcl_ros/point_cloud.h"
 #include "pcl_conversions/pcl_conversions.h"
 #include "sensor_msgs/PointCloud2.h"
 #include "tf2_ros/static_transform_broadcaster.h"
+#include <tf/transform_listener.h>
+#include <string>
 #include <tf/transform_listener.h>
 
 
--- src/object_3d_estimation.cpp
+++ src/object_3d_estimation.cpp
@@ -1,10 +1,20 @@
 #include "object_3d_estimation.h"
-
-void cbNewPCl (const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& msg){
+std::string frame;
+pcl::PointCloud<pcl::PointXYZRGB> cloud_framed;
+void cbNewPCL(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& msg){
+   // tf::TransformListener listener;
+    //tf::StampedTransform transform;
+   // listener.lookupTransform("velodyne", frame, ros::Time(0), transform);
+    //transform pointcloud to target frame given from param
+   // pcl_ros::transformPointCloud(*msg, cloud_framed, transform);	
+   
+    //pub_cloud_XYZRGB.publish(cloud_framed);
+    std::cout << "wiwowi: " << std::endl;
 
 }
 void cbNewImage(const sensor_msgs::ImageConstPtr& msg)
 {
+    std::cout << "wiiiwowo" << std::endl;
     // Convert ros msg to opencv image
     cv_bridge::CvImageConstPtr cv_data;
     cv_data = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
@@ -15,19 +25,17 @@ void cbNewImage(const sensor_msgs::ImageConstPtr& msg)
 }
 
 int main(int argc, char** argv) {
-
     ros::init(argc, argv, "object_3d_estimation");
-
     ros::NodeHandle nh;
-    std::String frame;
-
-    image_transport::ImageTransport it(nh);
-    image_transport::Subscriber image_sub = it.subscribe("/stereo/left/image_rect_color", 1, cbNewImage);
-    ros::Subscriber cloud_sub = nh.subscribe<pcl::PointCloud<pcl::PointXYZRGB> ("velodyne_points", 1, cbNewPCL);
+ //   nh.getParam("/object_3d_estimation/frame_id", frame);
+    
+ //   pub_cloud_XYZRGB =  nh.advertise<PointCloudXYZRGB> ("points2", 1);
+ //   image_transport::ImageTransport it(nh);
+  //  image_transport::Subscriber image_sub = it.subscribe("/stereo/left/image_rect_color", 1, cbNewImage);
+  //  ros::Subscriber cloud_sub = nh.subscribe("/velodyne_points", 1, cbNewPCL);
 
     // se calhar faz sentido fazer sincronizaçao de topicos aqui para garantir que o depth map e calculado sempre para 
-    nh.getParam("/object_3d_estimation/frame_id", frame);
-
+    
     while(ros::ok())
     {
         ros::spinOnce();
